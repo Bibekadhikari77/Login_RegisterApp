75a452d2005a07191aba4fda1352ddfeb6deec8b
Bibek Adhikari <adhikaribibek84@gmail.com>
2025-06-23 17:22:06 +0545

    Added line number 13 in ForgotPasswordActivity file

--- app/src/main/java/com/example/login_registerapp/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/login_registerapp/ForgotPasswordActivity.kt
@@ -1,41 +1,20 @@
 package com.example.login_registerapp
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class ForgotPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_forgot_password)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        // Perform any necessary actions when the activity becomes visible
-    }
-
-    override fun onResume() {
-        super.onResume()
-        // Perform any necessary actions when the activity resumes from a paused state
-    }
-
-    override fun onPause() {
-        super.onPause()
-        // Perform any necessary actions when the activity is about to enter the background
-    }
-
-    override fun onStop() {
-        super.onStop()
-        // Perform any necessary actions when the activity is no longer visible
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        // Perform any necessary cleanup when the activity is destroyed
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        // Perform any necessary actions when the activity is restarted after being stopped
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
